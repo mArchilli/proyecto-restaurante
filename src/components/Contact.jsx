8333cbd7751b665450af3ae467be3d6e9ef50616
mArchilli <110573064+mArchilli@users.noreply.github.com>
2024-10-04 13:45:21 -0300

    cambio vista mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
     <section className="bg-gray-100 py-12" id="contact">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-8">Veni a visitarnos</h2>
-        <div className="flex flex-col md:flex-row items-start space-y-6 md:space-y-0 md:space-x-8" style={{ height: '300px' }}>
+        <div className="flex flex-col md:flex-row items-start space-y-6 md:space-y-0 md:space-x-8">
           <div className="w-full md:w-1/2 h-full">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3081.5091599336915!2d-0.39175340000000003!3d39.435222499999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd604edd225db98f%3A0x6316b22c4050cf1d!2sTasca%20La%20Fundicion!5e0!3m2!1ses-419!2sar!4v1727796211063!5m2!1ses-419!2sar"
@@ -13,7 +13,6 @@ export default function Contact() {
               allowFullScreen
               loading="lazy"
               title="Ubicacion de Restaurante"
-              className="rounded"
             ></iframe>
           </div>
           <div className="w-full md:w-1/2 bg-white p-6 rounded-lg shadow-md h-full">
