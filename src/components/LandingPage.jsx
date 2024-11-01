97053c1744750a001097b22bca9253157e9fdf87
mArchilli <110573064+mArchilli@users.noreply.github.com>
2024-11-01 11:57:56 -0300

    animaciones implementadas

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import About from '../components/About';
@@ -6,13 +10,26 @@ import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
 const LandingPage = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Duración de las animaciones en milisegundos
+      once: true,     // Si `true`, la animación se activa solo una vez al hacer scroll
+    });
+  }, []);
+
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
       <Header />
-      <About />
-      <GridMenu />
-      <Contact />
+      <div data-aos="fade-right">
+        <About />
+      </div>
+      <div data-aos="fade-right">
+        <GridMenu />
+      </div>
+      <div data-aos="fade-right">
+        <Contact />
+      </div>
       <Footer />
     </div>
   );
