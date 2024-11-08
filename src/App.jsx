16833388dffcfdd6822ee155336e07bcf4b05329
mArchilli <110573064+mArchilli@users.noreply.github.com>
2024-11-08 09:34:05 -0300

    cambio en nombre de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import Login from './components/login';
+import Login from './components/Login';
 import UploadImages from './components/uploadImages';
 import ProtectedRoute from './components/ProtectedRoute';
 import NotFound from './components/NotFound';
